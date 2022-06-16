167cec09bbd0bf6794a6f4d06421da30ea5105ce
JintekiWarrior <jintekiwarrior@gmail.com>
2022-06-16 09:32:41 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+    // Character Class
+        // Health
+        // Attack
+        // Defense
+        // Mana
+        // Speed = In which order can characters start?
+    // Mage Class extends Character
+        // Spells
+        // Shield
+    // Monster Class extends Character
+        // Spells
+        // Rage
+    // Hero is the user.
+        // UserName
+        // Move Characters()
+        // Choose Action()
+    // AI
+        // Move Characters()
+        // Choose Action()
+    // Board
+        // gameBoard
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

b2c91e9c2c1683ad5abbd2a200ee9c84cc099766
JintekiWarrior <jintekiwarrior@gmail.com>
2022-06-20 11:15:17 -0400

    Add class and functions to create a game board

--- src/java/GameBoard/GameBoard.java
+++ src/java/GameBoard/GameBoard.java
@@ -1,7 +1,7 @@
 package GameBoard;
 
 //        System.out.println(" ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ ");
-//        System.out.println("|   |   |   |   |   |   |   |   |   |   |");
+//        System.out.println("|   | 😀 |   |   |   |   |   |   |   |   |");
 //        System.out.println("|___|___|___|___|___|___|___|___|___|___|");
 //        System.out.println("|   |   |   |   |   |   |   |   |   |   |");
 //        System.out.println("|___|___|___|___|___|___|___|___|___|___|");
@@ -18,22 +18,26 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameBoard {
-    public ArrayList<String> gameBoard;
+    public String gameBoard;
 
     public ArrayList<String> gameBoardRowList;
 
+    public ArrayList<ArrayList <String>> gameBoardList;
+
     public GameBoard()
     {
-        gameBoardRowList = gameBoardRowListCreate();
+        gameBoardList = this.gameBoardListCreate();
+
     }
 
     // creates a row of squares in the gameBoard
     public ArrayList<String> gameBoardRowListCreate()
     {
-        gameBoardRowList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
         // for loop to generate the start
         // for loop to generate the middle
         // for loop to generate the bottom
@@ -42,27 +46,64 @@ public class GameBoard {
         String bottom = "|___|";
         for (int i = 0; i < 10; i++)
         {
-            gameBoardRowList.add(top);
-            gameBoardRowList.add(middle);
-            gameBoardRowList.add(bottom);
+            result.add(top);
+            result.add(middle);
+            result.add(bottom);
         }
-        Collections.sort(gameBoardRowList);
+        Collections.sort(result);
 
-        return gameBoardRowList;
+        return result;
+    }
+
+    // method to join the array list back to a string of blocks.
+    public String joinArrayList(ArrayList<String> gameBoardRowList, int index)
+    {
+        List<String> gameBoardTopList = gameBoardRowList.subList(0, 9);
+        List<String> gameBoardMidList = gameBoardRowList.subList(10, 19);
+        List<String> gameBoardBotList = gameBoardRowList.subList(20, 29);
+
+        String gameBoardTop = String.join("", gameBoardTopList);
+        String gameBoardMid = String.join("", gameBoardMidList);
+        String gameBoardBot = String.join("", gameBoardBotList);
+
+        // only the top row needs to have a top.
+        if (index == 0) return gameBoardTop + "\n" + gameBoardMid + "\n" + gameBoardBot;
+        else return gameBoardMid + "\n" + gameBoardBot;
+    }
+
+    // method to create the list of the gameBoard
+    public ArrayList gameBoardListCreate()
+    {
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        for (int i = 0; i < 8; i++)
+        {
+            result.add(gameBoardRowListCreate());
+        }
+        return result;
+    }
+
+    public String gameBoardCreate()
+    {
+        String result = "";
+        for (int i = 0; i < 7; i++)
+        {
+            result = result + this.joinArrayList(gameBoardList.get(i), i) + "\n";
+        }
+        return result;
     }
 
     // this updates the middle item in a block in a row
     // column should be a number 1 - 10 for the blocks in a row
-    public void updateRow(int column, char item)
+    public void gameBoardRowListUpdate(int column, String item)
     {
         column += 10;
-        char[] blockArray = gameBoardRowList.get(column).toCharArray();
-        Array.setChar(blockArray, 2, item);
-        String updatedBlock = String.valueOf(blockArray);
+        String[] blockArray = gameBoardRowList.get(column).split("");
+        blockArray[2] = item;
+        String updatedBlock = String.join("", blockArray);
         gameBoardRowList.set(column, updatedBlock);
     }
 
-    public ArrayList getGameBoard() {
+    public String getGameBoard() {
         return gameBoard;
     }
 
@@ -70,7 +111,11 @@ public class GameBoard {
         return gameBoardRowList;
     }
 
-    public void setGameBoard(ArrayList gameBoard) {
+//    public ArrayList<String> getGameBoardList() {
+//        return gameBoardList;
+//    }
+
+    public void setGameBoard(String gameBoard) {
         this.gameBoard = gameBoard;
     }
 
--- src/java/GameBoard/Main.java
+++ src/java/GameBoard/Main.java
@@ -1,12 +1,18 @@
 package GameBoard;
 
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+
 public class Main
 {
     public static void main(String[] args)
     {
         GameBoard board = new GameBoard();
-        System.out.println(board.getGameBoardRowList());
-        board.updateRow(3, 'O');
-        System.out.println(board.getGameBoardRowList());
+        System.out.println(board.gameBoardCreate());
     }
 }
